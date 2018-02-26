b4952a02ab6060a057c7e6ea705f1b93b83156dd
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-02-25 22:21:04 -0600

    Basic listening of random number in Cloud Firestore up and running.

--- VaporFirebaseDemo-ClientApp/ViewController.swift
+++ VaporFirebaseDemo-ClientApp/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private lazy var db = Firestore.firestore()
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupFirestoreRandomNumberListener()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Setup
+    
+    private func setupFirestoreRandomNumberListener() {
+        db.collection("randomNumbers").document("theRandomNumber").addSnapshotListener { (snapshot, error) in
+            guard let document = snapshot else {
+                debugPrint("Error fetching random number document: \(String(describing: error))")
+                return
+            }
+            
+            debugPrint(document.data())
+        }
     }
-
-
 }
-
