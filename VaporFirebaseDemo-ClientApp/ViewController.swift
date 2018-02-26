0a785bccf54226870ef4bdccd728eeae1b1994ab
Tyler Milner <tylermilner@gmail.com>
2018-02-25 21:49:59 -0600

    Initial Xcode project generation.

---
+++ VaporFirebaseDemo-ClientApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VaporFirebaseDemo-ClientApp
+//
+//  Created by Tyler Milner on 2/25/18.
+//  Copyright © 2018 Tyler Milner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
